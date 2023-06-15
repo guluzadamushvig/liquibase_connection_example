7c080293a7b6865bdf0d2f3e1f8f04beb4425d8e
Mushvig <guluzadamushvig@gmail.com>
2023-06-15 21:22:24 +0400

    add liquibase config

---
+++ src/main/java/com/example/liguibase/liguibase/LiguibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.liguibase.liguibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiguibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiguibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/liguibase/liguibase/LiguibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liguibase.liguibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiguibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
